a873ae986bc143eb4769b2d7af0118064c1a9efe
Sam <sam@malpass.me.uk>
2018-02-09 22:00:13 +0000

    Add Credits function to Menu

--- Maze-Game/Menu.cpp
+++ Maze-Game/Menu.cpp
@@ -1,2 +1,20 @@
 /*Header Files*/
 #include "Menu.h"
+/*Function Definition - Credits*/
+void Credits()
+{
+	/*Clear Console Window*/
+	system("cls");
+	/*Open File*/
+	ifstream Open("Credits.txt");
+	/*Initializes a String with Data from Function Call*/
+	string Credits = ReadFile(Open);
+	/*Outputs String to Console*/
+	cout << Credits << endl;
+	/*Close File*/
+	Open.close();
+	/*Wait for a Set Perion of Time*/
+	Sleep(2500);
+	/*Returns to the Main Menu*/
+	return;
+}
