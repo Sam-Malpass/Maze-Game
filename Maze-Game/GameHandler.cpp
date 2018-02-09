0c5155700611969f678432ccc59443ebe6ed95d1
Sam <sam@malpass.me.uk>
2018-02-09 21:52:37 +0000

    Add StartGame function to GameHandler

--- Maze-Game/GameHandler.cpp
+++ Maze-Game/GameHandler.cpp
@@ -4,3 +4,25 @@
 char Maze[HEIGHT][WIDTH];
 int Score = 0, Moves;
 bool IsFinished, IsEscaped;
+/*Function Definition - StartGame*/
+void StartGame()
+{
+	/*Variable Declaration*/
+	int StartX, StartY;
+	IsEscaped = false;
+	/*While Escape Key not Pressed*/
+	while (IsEscaped == false)
+	{
+		/*Generate Maze*/
+		Generator();
+		/*Print Maze*/
+		PrintMaze(Maze, Score);
+		/*Determine Starting Co-ordinates*/
+		StartY = FindStartY(Maze);
+		StartX = FindStartX(Maze);
+		/*Begin Accepting Inputs*/
+		Inputting(Maze, StartY, StartX);
+	}
+	/*Return when Finished*/
+	return;
+}
