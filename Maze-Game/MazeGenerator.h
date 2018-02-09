eec95eab73232d1501c2f6e726cfb49b57867ece
Sam <sam@malpass.me.uk>
2018-02-09 21:41:26 +0000

    Declare functions in MazeGenerator

--- Maze-Game/MazeGenerator.h
+++ Maze-Game/MazeGenerator.h
@@ -4,5 +4,11 @@
 #include <stack>
 #include <ctime>
 #include <Windows.h>
-#include "Game Handler.h"
+#include "GameHandler.h"
 #include "Utility.h"
+/*Function Declaration*/
+int GenerateLeftRight(int Direction, int X);
+int GenerateUpDown(int Direction, int Y);
+bool IsGenerationGood(int X, int Y, int Direction, char Maze[HEIGHT][WIDTH]);
+void Generator();
+/*Inspired by this Algorithm http://stackoverflow.com/questions/8820993/recursive-backtracker-maze-generation-algorithm-stack-loop */
