c9a0afb7d19518ee20ad767d40503e4d2a41be2f
Sam <sam@malpass.me.uk>
2018-02-09 21:54:27 +0000

    Add Inputting function to GameHandler

--- Maze-Game/GameHandler.cpp
+++ Maze-Game/GameHandler.cpp
@@ -62,3 +62,100 @@ int FindStartX(char Maze[HEIGHT][WIDTH])
 		}
 	}
 }
+/*Function Definition - Inputting*/
+void Inputting(char Maze[HEIGHT][WIDTH], int Y, int X)
+{
+	/*Variable Setting*/
+	IsFinished = false;
+	Moves = 0;
+	/*While Maze is Unfinished*/
+	while (IsFinished == false)
+	{
+		/*Variable Declaration*/
+		bool Able = false;
+		char Key;
+		/*Get User's Input Without Enter*/
+		Key = _getch();
+		/*Checks Key and Performs Completion Check*/
+		if (Key == 'w' || Key == 'W')
+		{
+			/*Determines Whether Movement is Possible*/
+			Able = MovementCheck(Maze, Y, X, Key);
+			/*Checks if Able*/
+			if (Able == true)
+			{
+				/*Sets the Postion Being Moved to as the Player Counter*/
+				Maze[Y - 1][X] = 'O';
+				/*Changes Current Positon to Path*/
+				Maze[Y][X] = ' ';
+				/*Changes Coordinates for Current Position*/
+				Y = Y - 1;
+				/*Refreshes the Output*/
+				PrintMaze(Maze, Score);
+			}
+		}
+		/*Checks Key and Performs Completion Check*/
+		if (Key == 'a' || Key == 'A')
+		{
+			/*Determines Whether Movement is Possible*/
+			Able = MovementCheck(Maze, Y, X, Key);
+			/*Checks if Able*/
+			if (Able == true)
+			{
+				/*Sets the Postion Being Moved to as the Player Counter*/
+				Maze[Y][X - 1] = 'O';
+				/*Changes Current Positon to Path*/
+				Maze[Y][X] = ' ';
+				/*Changes Coordinates for Current Position*/
+				X = X - 1;
+				/*Refreshes the Output*/
+				PrintMaze(Maze, Score);
+			}
+		}
+		/*Checks Key and Performs Completion Check*/
+		if (Key == 's' || Key == 'S')
+		{
+			/*Determines Whether Movement is Possible*/
+			Able = MovementCheck(Maze, Y, X, Key);
+			/*Checks if Able*/
+			if (Able == true)
+			{
+				/*Sets the Postion Being Moved to as the Player Counter*/
+				Maze[Y + 1][X] = 'O';
+				/*Changes Current Positon to Path*/
+				Maze[Y][X] = ' ';
+				/*Changes Coordinates for Current Position*/
+				Y = Y + 1;
+				/*Refreshes the Output*/
+				PrintMaze(Maze, Score);
+			}
+		}
+		/*Checks Key and Performs Completion Check*/
+		if (Key == 'd' || Key == 'D')
+		{
+			/*Determines Whether Movement is Possible*/
+			Able = MovementCheck(Maze, Y, X, Key);
+			/*Checks if Able*/
+			if (Able == true)
+			{
+				/*Sets the Postion Being Moved to as the Player Counter*/
+				Maze[Y][X + 1] = 'O';
+				/*Changes Current Positon to Path*/
+				Maze[Y][X] = ' ';
+				/*Changes Coordinates for Current Position*/
+				X = X + 1;
+				/*Refreshes the Output*/
+				PrintMaze(Maze, Score);
+			}
+		}
+		/*If Escape Key Pressed*/
+		if (Key == 27)
+		{
+			/*Change Boolean Variables*/
+			IsFinished = true;
+			IsEscaped = true;
+			/*Return Through Functions to Menu*/
+			return;
+		}
+	}
+}
